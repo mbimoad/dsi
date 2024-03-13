7ff7541cb4a2eb74ef1ed41cce4864be50ecd092
mbimoad <bimoadjie19@gmail.com>
2024-03-13 08:14:40 -0700

    update mission & values

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -6,13 +6,14 @@ if(window.innerWidth <= 785) {
     cards.classList.add('owl-carousel');
     cards.classList.add('owl-theme');
     cards.classList.add('owl4');
+    
+    $('.owl4').owlCarousel({
+        items:1,
+    })
 
     
 }
 
-$('.owl4').owlCarousel({
-    items:1,
-})
 
 if(window.innerWidth > 785) {
     card.forEach(item => item.addEventListener('mouseover', function() {
