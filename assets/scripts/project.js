19dd13f53189e46eb4820e8ccf9e304d9629bb05
mbimoad <bimoadjie19@gmail.com>
2024-03-17 21:52:17 +0700

    update date and time

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -1,3 +1,56 @@
+function GetDate() {
+    // Create a new Date object for the current date
+    var currentDate = new Date();
+
+    // Array of month names
+    var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+    ];
+
+    // Get the day, month, and year
+    var day = currentDate.getDate();
+    var monthIndex = currentDate.getMonth();
+    var year = currentDate.getFullYear();
+
+    // Format the date
+    var formattedDate = day + ' ' + monthNames[monthIndex] + ' ' + year;
+
+    document.querySelector('.timestamp .date span:last-child').innerText = formattedDate; 
+}
+
+
+function GetTime() {
+    // Create a new Date object for the current time
+    var currentTime = new Date();
+
+    // Get the hour, minute, and second
+    var hour = currentTime.getHours();
+    var minute = currentTime.getMinutes();
+    var second = currentTime.getSeconds();
+
+    // Add leading zeros if needed
+    hour = (hour < 10 ? "0" : "") + hour;
+    minute = (minute < 10 ? "0" : "") + minute;
+    second = (second < 10 ? "0" : "") + second;
+
+    // Format the time
+    var formattedTime = hour + ":" + minute + ":" + second + " WIB";
+
+    document.querySelector('.timestamp .time span:last-child').innerText = formattedTime; 
+
+}
+
+setInterval(() => {
+    GetTime();
+}, 1000)
+
+GetDate();
+
+
+
 const pinImage = document.querySelectorAll('.pinImage');
 
 pinImage.forEach(item => item.addEventListener('click', function() {
