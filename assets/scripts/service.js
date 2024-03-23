1e51839bf7a5ad421a6a43005389159f62e9b747
mbimoad <bimoadjie19@gmail.com>
2024-03-23 12:40:50 +0700

    update

--- assets/scripts/service.js
+++ assets/scripts/service.js
@@ -1,5 +1,29 @@
 const togglebox = document.querySelectorAll('.service-togglebox .boxs .box'); 
-
+let tmp = "";
 togglebox.forEach(item => item.addEventListener('click', function() {
-    this.classList.toggle('closed');
+    if(!this.classList.contains('closed')) {
+        tmp.classList.add('closed');
+        return
+    }
+
+    if (tmp) {
+        console.log("sini");
+        tmp.classList.add('closed');
+        tmp = ""; 
+    }
+    if(!tmp) tmp = this; 
+    
+    this.classList.remove('closed');
+    
 }))
+
+
+window.onload = function() {
+    if(window.innerWidth <= 570) {
+        $('.service-togglebox .icon-boxs').owlCarousel({
+            items:1,
+            center:true
+        })
+    }
+}
+ 
