e8837c2fa47ecec0a375e1aaed8eda7e4a109886
mbimoad <bimoadjie19@gmail.com>
2024-03-30 09:56:57 +0700

    upate mobile mode for contact

--- assets/scripts/careersdetail.js
+++ assets/scripts/careersdetail.js
@@ -6,15 +6,21 @@ document.addEventListener('click', function(e) {
 
     if(e.target.classList.contains('btnapply')) {
         let element = e.target; 
-        document.querySelector('.box-white').classList.toggle('active'); 
-        document.querySelector('.overlay').classList.toggle('active'); 
+        document.querySelector('.box-white').classList.add('active'); 
+        document.querySelector('.overlay').classList.add('active'); 
     }
 
     if(e.target.classList.contains('overlay')) {
         let element = e.target; 
-        document.querySelector('.box-white').classList.toggle('active'); 
-        document.querySelector('.overlay').classList.toggle('active'); 
+        document.querySelector('.box-white').classList.remove('active'); 
+        document.querySelector('.overlay').classList.remove('active'); 
     }
+   
 })
 
 
+
+document.querySelector('.closed svg').addEventListener('click', function() {
+    document.querySelector('.box-white').classList.remove('active'); 
+    document.querySelector('.overlay').classList.remove('active'); 
+})
